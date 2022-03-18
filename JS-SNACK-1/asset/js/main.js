5c25004d3c1ac5d33b01e3369e52524f8e0e6e08
SimonePavone <simonebest56@gmail.com>
2022-03-18 09:35:41 +0100

    Esercizi terminati

--- JS-SNACK-1/asset/js/main.js
+++ JS-SNACK-1/asset/js/main.js
@@ -5,21 +5,35 @@ inserirli nell’array fino a quando la somma degli elementi è minore di 50.
 
 Snack2
 Fai inserire un numero, che chiameremo N, all’utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. 
-Ogni volta che ne crei uno, stampalo.ù
+Ogni volta che ne crei uno, stampalo.
 Snack3
 Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
 Snack4 (Bonus)
 In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o 
 no alla festa. Interrompi il ciclo appena il nome è stato trovato.*/
 
-let aray1 = [];
+let array = [];
+console.log(array);
 
-let numeroRichiesto = parseInt (prompt("Scrivi un numero"));
+// creo la variabile somma vuota a zero
+let somma = 0;
+console.log(somma);
 
+// ciclo while che funziona fino a che somma è inferiore a 50
+while (somma < 50) {
 
-while ( numeroRichiesto < 50 ) {
-    let numeroRichiesto = parseInt (prompt("Scrivi un numero"));
+    // prompt per chiedere a utente di inserire numero
+    let numeroUtente = parseInt(prompt("inserisci un numero"));
+    console.log(numeroUtente);
+
+    // aggiungo il numero chiesto nell array vuoto
+    array.push(numeroUtente);
+    console.log(array);
+
+    // sommo tutti i numeri chiesti fino a che non supera il 50
+    somma += numeroUtente;
 
 }
 
-console.log(numeroRichiesto)
+// stampa del valore finale
+console.log(somma);
---
+++ JS-SNACK-2/asset/js/main.js
@@ -0,0 +1,29 @@
+let N = parseInt(prompt("inserisci un numero"));
+
+// creo un array vuota che deve contenere N array
+let Narray = [];
+
+// creo un ciclo che funziona fino a quando i < N ed a ogni ciclo incremento di 1
+for ( i = 0; i < N ; i++ ) {
+    
+    // ad ogni ciclo creo una array tipo vuota
+    let arrayTipo = [];
+    
+    // ciclo while fino a quando l'array tipo ha dentro numeri < 10
+    while ( arrayTipo.length < 10 ) {
+
+        // generatore di numeri random da 1 a 100
+        arrayTipo.push(Math.floor(Math.random() * 100) + 1 );
+
+    }
+    
+    // stampa degli array tipo singoli
+    console.log(arrayTipo);
+
+    // inserimento degli N array dentro ad una variabile
+    Narray.push(arrayTipo);
+
+}
+
+// stampa finale delle N array
+console.log(Narray);
